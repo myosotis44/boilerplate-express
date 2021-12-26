feac85088e3d65bd98f61483b7c45acc7e0f59f0
myosotis44 <victoria.gouin@gmail.com>
2021-12-26 17:49:43 +0100

    Envoyer un fichier HTML comme réponse aux requêtes GET sur le chemin / (root)

--- myApp.js
+++ myApp.js
@@ -5,8 +5,18 @@ console.log('Hello World');
 
 // Start a Working Express Server
 
+/*
+
 app.get('/', (req, res) => {
     res.send('Hello Express');
+});
+
+*/
+
+// Serve an HTML file -- Send the index.html file as a response to GET requests to the / path -- 
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
